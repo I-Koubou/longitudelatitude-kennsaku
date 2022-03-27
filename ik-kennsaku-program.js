a45e8986b98674a6ade69099f065e8397df8dee2
IBK2628 (I-Koubou) <92432744+I-Koubou@users.noreply.github.com>
2022-03-27 12:47:54 +0900

    Add files via upload

---
+++ ik-kennsaku-program.js
@@ -0,0 +1,18 @@
+function alerthyo() {
+    const keidojs = document.getElementById("keido");
+    const keido2 = keidojs.value;
+   
+    const idojs = document.getElementById("ido");
+    const ido2 = idojs.value;
+    
+    const dasyo = keido2 + ',' + ido2
+    if(dasyo == "," ) {alert("緯度と経度を入力して下さい");
+    }else{
+        var result = confirm(dasyo+"をGoogleマップで検索します。");
+        if( result ) {
+            const url = 'https://www.google.com/maps/search/?api=1&query='+dasyo
+            window.open(url)
+
+        }
+    }
+}
