9e469f1e0894b300051380f5fa41b4fcc56f39e9
ibui2628 (I-Koubou) <92432744+I-Koubou@users.noreply.github.com>
2022-03-28 14:35:23 +0900

    Update ik-kennsaku-program.js

--- ik-kennsaku-program.js
+++ ik-kennsaku-program.js
@@ -1,28 +1,24 @@
 var coun = 0;
 function alerthyo() {
-    const keidojs = document.getElementById("keido");
-    const keido2 = keidojs.value;
-    const idojs = document.getElementById("ido");
-    const ido2 = idojs.value;
-    const dasyo = keido2 + ',' + ido2
-    if (keido2 > "90") {
-      alert ("経度が90゜以上です。")
-    } else if (ido2 > "180") {
-      alert ("緯度が180゜以上です。")
-    } else {
+  const idojs = document.getElementById("ido");
+  const ido2 = idojs.value;
+  const keijs = document.getElementById("kei");
+  const kei2 = keijs.value;
+  const dasyo = ido2 + ',' + kei2
       if (dasyo == ",") {
         alert("緯度と経度を入力して下さい");
       } else {
         coun += 1;
         const url = 'https://www.google.com/maps/search/?api=1&query=' + dasyo
-          document.getElementById("kotae").style.visibility="visible";
-          var textbox_element = document.getElementById('urltxt');
-          var new_element = document.createElement('p');
-          new_element.textContent = "カウント:"+coun + " URL:" + url;
-          textbox_element.appendChild(new_element);
-          
-          let link = document.getElementById('map');
-          link.setAttribute('href', url);
-        }
+        document.getElementById("kotae").style.visibility = "visible";
+        //URL表示プロセス
+        var textbox_element = document.getElementById('urltxt');
+        var new_element = document.createElement('p');
+        new_element.textContent = "カウント:" + coun + " URL:" + url;
+        textbox_element.appendChild(new_element); //URLを表示
+        //mapリンク表示プロセス
+        let link = document.getElementById('map');
+        link.setAttribute('href', url);
+      }
     }
-  }
+  
